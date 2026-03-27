6596766bdd9ce1fce1e4a8de519a217e1edd0272
lpr1596@gmail.com <lpr@MacBook-Air-de-Lucas.local>
2026-03-27 11:16:06 -0300

    commit inicial

---
+++ src/test/java/com/notificacoes_email/notificacoes/NotificacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificacoes_email.notificacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
